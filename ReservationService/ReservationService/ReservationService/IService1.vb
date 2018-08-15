97b9e0e755a73ad0153d4c60241b132a7383c433
Christopher Henry Simmons <Christopher.Simmons@protonmail.ch>
2018-08-15 15:59:35 +1200

    Update End Class

---
+++ ReservationService/ReservationService/ReservationService/IService1.vb
@@ -0,0 +1,11 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the interface name "IService1" in both code and config file together.
+<ServiceContract()>
+Public Interface IService1
+
+    <OperationContract()>
+    Function Reserve(ByVal seatType As String, ByVal classType As String) As Boolean
+
+
+
+End Interface
+
---
+++ ReservationService/ReservationService/ReservationService/Service1.svc.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Service1
+    Implements IService1
+
+    Public Function Reserve(ByVal seatType As String, ByVal classType As String) As Boolean Implements IService1.Reserve
+
+        Dim objConnection As SqlConnection
+        Dim objCommand As SqlCommand
+        Dim retValue As Boolean
+
+        'change
+        objConnection = New SqlConnection("Data Source=PE203-30\MSSQLSVR;Initial Catalog=TICKETS;Integrated Security=True")
+        objCommand = New SqlCommand("Reserve", objConnection)
+        objCommand.CommandType = CommandType.StoredProcedure
+
+        Dim objParameter1 As New SqlParameter("@seatType", SqlDbType.VarChar, 50)
+        objCommand.Parameters.Add(objParameter1)
+        objParameter1.Direction = ParameterDirection.Input
+        objParameter1.Value = seatType
+
+        Dim objParameter2 As New SqlParameter("@classType", SqlDbType.VarChar, 50)
+        objCommand.Parameters.Add(objParameter2)
+        objParameter2.Direction = ParameterDirection.Input
+        objParameter2.Value = classType
+
+        Dim objOutputParameter As New SqlParameter("@Response", SqlDbType.Bit)
+        objCommand.Parameters.Add(objOutputParameter)
+        objOutputParameter.Direction = ParameterDirection.Output
+
+        objConnection.Open()
+
+        objCommand.ExecuteNonQuery()
+        retValue = objCommand.Parameters("@Response").Value
+        objConnection.Close()
+
+        If retValue Then Return True
+
+        Return False
+
+
+    End Function
+
+End Class
